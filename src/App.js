20ae20992e3f53ac0e1ab8833b9ee1b3adf75973
RamyaJarugulla7 <ramya.jarugulla@techdenali.com>
2026-04-24 18:30:23 +0530

    Added hero banner component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/Accordion';
 import AEMCarousel from './components/AEMCarousel';
+import HeroBanner from './components/HeroBanner';
 
 function App() {
   return (
     <div className="App">
       <AEMCarousel />
+      <HeroBanner />
       <Accordion />
     </div>
   );
